e5016e6f40ed3a9d43322a5785e93f525ef7a461
Stevan Popovic <stevanpopovic5@gmail.com>
2018-06-04 12:03:30 +0100

    1.2.4.1-6 created authentication with BCrypt in User model

--- models/user.js
+++ models/user.js
@@ -1,11 +1,41 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {type: String, required: true},
-  email: {type: String, required: true}
-  // password: { type: String, required: true }
+  email: {type: String, required: true},
+  password: { type: String, required: true }
 }, {
   timestamps: true
+});
+
+// setup virtual schema for password confirmation
+userSchema
+  .virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation) {
+    // temp store the password on the user model so we can check it
+    this._passwordConfirmation = passwordConfirmation;
+  });
+
+// lifecycle hooks on authentication
+// set up a pre-validate hook
+userSchema.pre('validate', function checkPassword(next) {
+  // check if password DOES NOT equal password
+  if(this.isModified('password') && this._passwordConfirmation !== this.password) this.invalidate('passwordConfirmation', 'does not match');
+
+  // otherwise continue validation
+  next();
+});
+
+userSchema.pre('save', function hashPassword(next) {
+  // check if the password has been modified
+  if(this.isModified('password')) {
+    // if true, hash the password with bcrypt and store the hashed password on the user object
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+
+  // continue to save password
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
