28d7e587dcffb283ee9f6b78b14383fc5b2b3e89
Stevan Popovic <stevanpopovic5@gmail.com>
2018-06-04 10:05:36 +0100

    1.8.1-6 - created environment.js in config for dbURI and basic mongoose setup

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const router = require('./config/router');
+const mongoose = require('mongoose');
 
 //Called packages
 const app = express();
 
 // Used packages & Other bits
-const PORT = process.env.PORT || 3000;
+const { port, dbURI } = require('./config/environment');
+// const PORT = process.env.PORT || 3000; no longer needed because of above line
 
+mongoose.connect(dbURI);
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 app.use(expressLayouts);
@@ -17,4 +20,4 @@ app.use(express.static(`${__dirname}/public`));
 
 //Last line always
 app.use(router);
-app.listen(PORT, () => console.log(`App is runnig on ${PORT}`));
+app.listen(port, () => console.log(`App is runnig on ${port}`));
