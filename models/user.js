85030fa25fca0690838c0c8112a7818f76ad06b1
Stevan Popovic <stevanpopovic5@gmail.com>
2018-06-05 11:25:17 +0100

    5.4. Basic styling of User index page

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {type: String, required: true},
+  image: {type: String, required: true},
   email: {type: String, required: true},
   password: { type: String, required: true }
 }, {
