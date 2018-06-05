ff6e300e0131074d20bb87654cf977596f5912e1
Stevan Popovic <stevanpopovic5@gmail.com>
2018-06-05 15:37:02 +0100

    5.6. Added styling to book form page

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   name: {type: String, required: true},
   image: {type: String, required: true},
   email: {type: String, required: true},
+  bio: {type: String, minlength: 100},
   password: { type: String, required: true }
 }, {
   timestamps: true
